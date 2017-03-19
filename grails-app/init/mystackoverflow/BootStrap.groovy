0a64c842a32e61fd8dadf4b3b9e960680dda79ff
nucktrooper <nucktrooper@gmail.com>
2017-03-19 13:27:24 +0100

    'feature_flip'

--- grails-app/init/mystackoverflow/BootStrap.groovy
+++ grails-app/init/mystackoverflow/BootStrap.groovy
@@ -1,5 +1,7 @@
 package mystackoverflow
 
+import groovy.json.JsonSlurper
+
 class BootStrap {
 
     def init = { servletContext ->
@@ -10,6 +12,27 @@ class BootStrap {
 
         5.times { new Question(title: "Question ${it+1}", views: 0, text: "Content of the question ${it+1}", user: user).save() }
 
+		// Read the file config.json in grails-app/conf
+		try
+		{
+			def resource = this.class.classLoader.getResource('config.json')
+			def configsFile = new File(resource.file)
+
+			// Set the configuration
+			def configsArray = new JsonSlurper().parseText(configsFile.text)
+			configsArray.each { key, value ->
+			    Feature.setActivated(key, value)
+			}
+		}
+		catch (Exception e)
+		{
+			println e
+		}
+
+		// Check the loaded values
+		println 'SignUp: ' + Feature.isActivated('SignUp')
+		println 'QuestionCreation: ' + Feature.isActivated('QuestionCreation')
+		println 'AnswerCreation: ' + Feature.isActivated('AnswerCreation')
     }
     def destroy = {
     }
